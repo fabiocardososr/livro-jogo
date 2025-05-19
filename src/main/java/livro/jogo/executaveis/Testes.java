20cde4d8fd9dd349c7f70d94af117f297586fb7b
fabio <fabiocardo@gmail.com>
2025-05-19 17:37:54 -0300

    Melhorando imagens através de IA.

--- src/main/java/livro/jogo/executaveis/Testes.java
+++ src/main/java/livro/jogo/executaveis/Testes.java
@@ -35,7 +35,7 @@ public class Testes {
         //System.out.println("Bolsa: "+ UtilBolsa.retornaListaDeBensNaBolsa().length);
 
         //TRABALHANDO NA SEÇÃO: (volte para 126)
-        Secao secao = DadosLivroCarregado.getLivro().getMapSecao().get( 104 );
+        Secao secao = DadosLivroCarregado.getLivro().getMapSecao().get( 7 );
         CarregarTelas.carregarSecao(secao);
 
 
