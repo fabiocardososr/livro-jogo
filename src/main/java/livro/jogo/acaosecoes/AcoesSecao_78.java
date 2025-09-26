de66622b8cc762dbe06ad0b39bae4fac184b6a49
fabio <fabiocardo@gmail.com>
2025-09-26 18:15:52 -0300

    correção

--- src/main/java/livro/jogo/acaosecoes/AcoesSecao_78.java
+++ src/main/java/livro/jogo/acaosecoes/AcoesSecao_78.java
@@ -2,12 +2,14 @@ package livro.jogo.acaosecoes;
 
 import livro.jogo.enums.ItensMapeamento;
 import livro.jogo.utils.DadosLivroCarregado;
+import livro.jogo.utils.Util;
 import livro.jogo.utils.UtilBolsa;
 
 public class AcoesSecao_78 {
 
     public static void incluirNaBolsaPoeiraDaLevitacao(){
         if (!UtilBolsa.verificarExistenciaDeItemNaBolsa(ItensMapeamento.POEIRA_DA_LEVITACAO.getIdItem())) {
+            new Util().reproduzirAudioMp3("audio/efeitos_sonoros/sorte.mp3", null);
             UtilBolsa.incluirItem(DadosLivroCarregado.getMapItem().get(ItensMapeamento.POEIRA_DA_LEVITACAO.getIdItem()));
         }
     }
